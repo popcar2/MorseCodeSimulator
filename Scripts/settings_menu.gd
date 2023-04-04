b5600d80bafecd91961527d9552b6ab95b6affe1
popcar2 <ahmoudy2000@gmail.com>
2023-04-04 23:02:46 +0200

    Add morse speed presets

--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -15,9 +15,32 @@ func _input(event):
 
 func _on_morse_speed_option_item_selected(index):
 	morse_speed = index
+	if morse_speed == 0:
+		%'Letter Time Spinbox'.value = 2.5
+		%'Word Time Spinbox'.value = 4
+		%'Long Press Time Spinbox'.value = 0.15
+	elif morse_speed == 1:
+		%'Letter Time Spinbox'.value = 2
+		%'Word Time Spinbox'.value = 3
+		%'Long Press Time Spinbox'.value = 0.15
+	elif morse_speed == 2:
+		%'Letter Time Spinbox'.value = 1
+		%'Word Time Spinbox'.value = 2
+		%'Long Press Time Spinbox'.value = 0.15
+	elif morse_speed == 3:
+		%'Letter Time Spinbox'.value = 0.5
+		%'Word Time Spinbox'.value = 1
+		%'Long Press Time Spinbox'.value = 0.15
+	elif morse_speed == 4:
+		%'Letter Time Spinbox'.value = 0.3
+		%'Word Time Spinbox'.value = 0.75
+		%'Long Press Time Spinbox'.value = 0.10
 
 func _on_letter_time_spinbox_value_changed(value):
 	letter_time = value
 
 func _on_word_time_spinbox_value_changed(value):
 	word_time = value
+
+func _on_long_press_time_spinbox_value_changed(value):
+	long_press_time = value
