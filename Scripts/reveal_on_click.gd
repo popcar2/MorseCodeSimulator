a299dfcec3de760188ccaeacf7456d1fcd6790e4
popcar2 <ahmoudy2000@gmail.com>
2023-04-08 15:23:29 +0200

    Add highlight when mouse over paper

--- Scripts/reveal_on_click.gd
+++ Scripts/reveal_on_click.gd
@@ -2,6 +2,15 @@ extends Panel
 
 var revealed: bool = true
 @onready var paper_click_audio: AudioStreamPlayer = $'Paper Click'
+@onready var panel_theme: StyleBoxFlat = get_theme_stylebox('panel').duplicate()
+
+func _ready():
+	panel_theme.border_color = Color('10c595')
+	panel_theme.border_width_left = 50
+	panel_theme.border_width_top = 50
+	panel_theme.border_width_right = 50
+	panel_theme.border_width_bottom = 50
+	add_theme_stylebox_override('panel', panel_theme)
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
@@ -24,3 +33,16 @@ func toggle_reveal():
 	else:
 		paper_click_audio.pitch_scale = 0.9
 	paper_click_audio.play()
+
+
+func _on_mouse_entered():
+	panel_theme.expand_margin_left = 7
+	panel_theme.expand_margin_bottom = 7
+	panel_theme.expand_margin_right = 7
+	panel_theme.expand_margin_top = 7
+
+func _on_mouse_exited():
+	panel_theme.expand_margin_left = 0
+	panel_theme.expand_margin_bottom = 0
+	panel_theme.expand_margin_right = 0
+	panel_theme.expand_margin_top = 0
